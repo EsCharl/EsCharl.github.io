0505b3cddc12cb53d69cfbcb2dd772fe5f40fd82
EsCharl <63916811+EsCharl@users.noreply.github.com>
2023-08-30 22:25:29 +0100

    adding old files

---
+++ src/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const imageLocation = [{name: "CBMW", location:"./images/CBMW.png", details:"My First Hackaton: \n Created an website that helps with cost of living crisis and information about the users credit score"}, {name: "dissertation", location: "./images/game_image.png", details: "Dissertation: \ncreated a snake game environment with multiple different path finding algorithms and one trained model using evolutionary algorithm and supervised learning methodology."}];
+
+function FadeInSection(props) {
+  const [isVisible, setVisible] = React.useState(false);
+  const domRef = React.useRef();
+  React.useEffect(() => {
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => setVisible(entry.isIntersecting));
+    });
+    observer.observe(domRef.current);
+  }, []);
+  return (
+    <div
+      className={`fade-in-section ${isVisible ? 'is-visible' : ''}`}
+      ref={domRef}
+    >
+      {props.children}
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div className="App">
+      {imageLocation.map(x => (
+        <FadeInSection>
+          <div classname={x.name}>
+            <div><img src= {x.location} alt={x.name}/></div>
+            <div><p>{x.details}</p></div>
+          </div>
+        </FadeInSection>
+      ))}
+    </div>
+  );
+}
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(<App />, rootElement);
