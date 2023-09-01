2c2b207165f9dbb293f8a305bf6b884efebd210c
EsCharl <63916811+EsCharl@users.noreply.github.com>
2023-09-02 00:46:14 +0100

    moving the files to main

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const imageLocation = [{name: "CBMW", location:"./images/CBMW.png", details:"My First Hackaton: \n Created an website that helps with cost of living crisis and information about the users credit score"}, {name: "dissertation", location: "./images/game_image.png", details: "Dissertation: \ncreated a snake game environment with multiple different path finding algorithms and one trained model using evolutionary algorithm and supervised learning methodology."}];
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function FadeInSection() {
+  const [isVisible, setVisible] = React.useState(false);
+  const domRef = React.useRef();
+  React.useEffect(() => {
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => setVisible(entry.isIntersecting));
+    });
+    observer.observe(domRef.current);
+  }, []);
+  return (
+    <div className={`fade-in-section ${isVisible ? 'is-visible' : ''}`} ref={domRef} />
+  );
+}
+
+export default function App() {
+  return (
+    <div className="App">
+      {imageLocation.map(x => (
+        <FadeInSection>
+          <div classname={x.name}>
+            <div><img src= {x.location} alt={x.name}/></div>
+            <div><p>{x.details}</p></div>
+          </div>
+        </FadeInSection>
+      ))}
+    </div>
+  );
+}
